db091566e878a10ec39c6603868215523386f943
ToanLeThanh <thanhtoanhcm1602@gmail.com>
2021-01-23 03:14:55 +0700

    - add props.

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem />
-        {/* <TodoItem />
-        <TodoItem /> */}
+        <TodoItem title = "model"/>
+        <TodoItem title = "controller"/>
+        <TodoItem title = "view"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -4,7 +4,7 @@ class TodoItem extends Component {
     render() {
         return(
             <div className="TodoItem">
-                <p>shopping</p>
+                <p>{this.props.title }</p>
             </div>
         );
     }
